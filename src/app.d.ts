d277e862b0bf9929d502434142617c9ba8ab7fb7
gregn <greg@gregnicol.uk>
2023-09-14 11:13:30 +0100

    Feature: Cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,16 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {}
 	// interface PageData {}
 	// interface Error {}
-	// interface Platform {}
+   interface Platform {
+       env: {
+           COUNTER: DurableObjectNamespace;
+       };
+       context: {
+           waitUntil(promise: Promise<any>): void;
+       };
+       caches: CacheStorage & { default: Cache }
+   }
 }
